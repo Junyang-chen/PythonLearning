bf8b97a0facfffd847cd46eafcccc3a0de9f6913
Tony <lightdarkness98@gmail.com>
2017-12-16 18:02:13 -0500

    Add comparison

--- PythonCookbook.py
+++ PythonCookbook.py
@@ -536,3 +536,68 @@ root.add_child(a)
 print(a.parent)
 del root
 print(a.parent)
+
+#8.24 class support comparison, only two method needs to be created the other will be created
+from functools import total_ordering
+
+
+class Room:
+    def __init__(self, name, length, width):
+        self.name = name
+        self.length = length
+        self.width = width
+        self.square_feet = length*width
+
+@total_ordering
+class House:
+    def __init__(self, name, style):
+        self.name = name
+        self.rooms = []
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    @property
+    def living_area(self):
+        return sum(i.square_feet for i in self.rooms)
+
+    def __str__(self):
+        return "House:{0} with square feet {1}".format(self.name, self.living_area)
+
+    def __eq__(self, other):
+        return self.living_area == other.living_area
+
+    def __lt__(self, other):
+        return self.living_area < other .living_area
+
+h1 = House('h1', 'Cape')
+h1.add_room(Room('Master Bedroom', 14, 21))
+h1.add_room(Room('Living Room', 18, 20))
+h1.add_room(Room('Kitchen', 12, 16))
+h1.add_room(Room('Office', 12, 12))
+h2 = House('h2', 'Ranch')
+h2.add_room(Room('Master Bedroom', 14, 21))
+h2.add_room(Room('Living Room', 18, 20))
+h2.add_room(Room('Kitchen', 12, 16))
+h3 = House('h3', 'Split')
+h3.add_room(Room('Master Bedroom', 14, 21))
+h3.add_room(Room('Living Room', 18, 20))
+h3.add_room(Room('Office', 12, 16))
+h3.add_room(Room('Kitchen', 15, 17))
+houses = [h1, h2, h3]
+print('Is h1 bigger than h2?', h1 > h2) # prints True
+print('Is h2 smaller than h3?', h2 < h3) # prints True
+print('Is h2 greater than or equal to h1?', h2 >= h1) # Prints False
+print('Which one is biggest?', max(houses)) # Prints 'h3: 1101-square-foot Split'
+print('Which is smallest?', min(houses)) # Prints 'h2: 846-square-foot Ranch'
+
+# 8.25 creating cached instance
+import logging
+
+a = logging.getLogger('foo')
+b = logging.getLogger('bar')
+
+print(a is b )
+c = logging.getLogger('foo')
+print(c is a)
+
